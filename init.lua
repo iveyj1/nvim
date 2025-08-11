d9c843a27727d4c40aef92032a72bd0e626510e8
iveyj <iveyj@hotmail.com>
2025-08-11 14:15:21 -0400

    add highlight yank

--- init.lua
+++ init.lua
@@ -58,3 +58,11 @@ require "mini.pick".setup()
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=none")
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Highlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})

ffaf8e14cae8e062da1063235dd96d8dd827693c
iveyj <iveyj@hotmail.com>
2025-10-13 14:47:08 -0400

    cleanup and add telescope, remove mini.pick

--- init.lua
+++ init.lua
@@ -1,28 +1,36 @@
---  Leader first
+
+--========================================================
+-- Basic Settings
+--========================================================
 vim.g.mapleader = " "
 
--- Options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.tabstop = 4
-vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.smartindent = true
 vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
 vim.opt.undofile = true
 vim.opt.incsearch = true
-vim.opt.signcolumn = "yes"
 vim.opt.undolevels = 10000
+
 -- Treesitter folds
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
+
+vim.opt.makeprg = [[./nrf-build.sh $*]]
+
+--========================================================
 -- Keymaps
+--========================================================
 local map = vim.keymap.set
 
 local function toggle_relativenumber()
@@ -30,112 +38,99 @@ local function toggle_relativenumber()
 end
 
 map('n', '<leader>rn', toggle_relativenumber, { desc = 'Toggle relative number' })
+map('n', '<leader>q', ':quit<CR>', { silent = true, desc = 'Quit' })
+map('n', '<leader>x', '<C-w>c', { silent = true, desc = 'Close window' })
+map('n', '<leader>ed', ':Oil<CR>', { silent = true, desc = 'Oil file explorer' })
+map('n', '<leader>ec', ':edit $MYVIMRC<CR>:only<CR>', { silent = true, desc = 'Edit config' })
+map('n', '<leader>ew', ':new<CR>:only<CR>', { silent = true, desc = 'Open new buffer' })
+map('n', '<leader>l', ':ls<CR>', { silent = true, desc = 'List buffers' })
+map('n', '<leader>w', '<C-w>j', { silent = true, desc = 'Next window' })
 
-vim.opt.makeprg = [[./nrf-build.sh -p . -b nrf54l15dk/nrf54l15/cpuapp -f -E "-DEXTRA_CONF_FILE=debug.conf" $*]]
-
-map('n', '<leader>q', ':quit<CR>',  { silent = true, desc = 'Quit' })
-map('n', '<leader>mr', ':MRU<CR>',  { silent = true, desc = 'MRU' })
-map('n', '<leader>x', '<C-w>c',     { silent = true, desc = 'Close window' })
-map('n', '<leader>ed', ':Oil<CR>',   { silent = true, desc = 'Oil file explorer' })
-map("n", "<leader>ec", ":edit $MYVIMRC<CR>:only<CR>", { silent = true, desc = "Open config as only window" })   
-map('n', '<leader>ew', ':new<CR>:only<CR>', { silent = true; desc = 'open new file as only window'})
-map('n', '<leader>l',  ':ls<CR>', {silent = true, desc = 'list buffers'})
-map('n', '<leader>thd',  ':TSDisable highlight<cr>', { silent = true, desc = "Disable Treesitter syntax highlighting"})
-map('n', '<leader>the',  ':TSEnsable highlight<cr>', { silent = true, desc = "Eisable Treesitter syntax highlighting"})
-map('n', '<leader>w',  '<c-w>j', { silent = true, desc = "Next window"})
-
-
--- System clipboard
-map({'n','v','x'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
-map({'n','v','x'}, '<leader>d', '"+d', { desc = 'Delete to system clipboard' })
-map('n', '<leader>p', '"+p',           { desc = 'Paste from system clipboard' })
+-- Clipboard
+map({'n','v','x'}, '<leader>y', '"+y', { desc = 'Yank to clipboard' })
+map({'n','v','x'}, '<leader>d', '"+d', { desc = 'Delete to clipboard' })
+map('n', '<leader>p', '"+p', { desc = 'Paste from clipboard' })
 
 -- Navigation niceties
-map({'n','v','x'}, '<C-d>', '<C-d>zz', { desc = 'Half-page down centered' })
-map({'n','v','x'}, '<C-u>', '<C-u>zz', { desc = 'Half-page up centered' })
-
--- Don’t use U as redo
+map({'n','v','x'}, '<C-d>', '<C-d>zz', { desc = 'Page down centered' })
+map({'n','v','x'}, '<C-u>', '<C-u>zz', { desc = 'Page up centered' })
 map('n', 'U', function() print("Do not use 'U'") end, { silent = true })
 
-
--- Plugins via pack (Neovim 0.12+)
+--========================================================
+-- Plugins (via vim.pack)
+--========================================================
 vim.pack.add({
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/echasnovski/mini.pick" },
     { src = "https://github.com/nvim-mini/mini.bufremove" },
     { src = "https://github.com/yegappan/mru" },
     { src = "https://github.com/folke/which-key.nvim" },
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- optional, icons
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" } ,
-    { src = "https://github.com/MunifTanjim/nui.nvim" } ,
-    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim" } ,
-    -- { src = "https://github.com/ibhagwan/fzf-lua" } ,
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+    { src = "https://github.com/MunifTanjim/nui.nvim" },
+    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim" },
 })
 
--- Pick (mini.pick)
-map({'n','v','x'}, '<leader>ff', ':Pick files<CR>', { silent = true, desc = 'Pick files from cwd' })
+--========================================================
+-- Telescope Setup
+--========================================================
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>fc', function()
-    MiniPick.builtin.files(nil, { source = { cwd = '../common' } })
-end, { desc = 'Open MiniPick in ../common' })
-
-vim.keymap.set('n', '<leader>fb', function()
-    MiniPick.builtin.files(nil, { source = { cwd = '../../boards/arm' } })
-end, { desc = 'Open MiniPick in ../boards/arm' })
-
-map({'n','v','x'}, '<leader>h', ':Pick help<CR>',  { silent = true, desc = 'Pick help' })
-map({'n','v','x'}, '<leader>b', ':Pick buffers<CR>', { silent = true, desc = 'Pick buffer'})
-
-vim.keymap.set('n', '<leader>x', function()
-   require('mini.bufremove').delete(0, false)
-   end, { desc = 'Delete buffer' })
-
-   --
--- map('n', '<leader>n', function() cycle_buffers(1, true) end, { silent = true })
-map('n', '<leader>n', ':bn<CR>', { silent = true, desc='Next buffer' })
-
-require("plugins.treesitter")
--- require("fzf-lua").files({ cmd = fd_cmd(roots, opts.args) })
-
--- optional: start with all folds open
--- vim.opt.foldlevel = 99
-require('oil').setup({ default_file_explorer = false, })
-require('mini.pick').setup()
-require('which-key').setup({
-    delay = 300,
-    show_help = true,
+telescope.setup({
+    defaults = {
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 8,
+        mappings = {
+            i = { ["<esc>"] = require('telescope.actions').close },
+        },
+    },
 })
+
+map('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'List buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+map('n', '<leader>fm', builtin.oldfiles, { desc = 'Recent files' })
+
+-- Project-specific Telescope shortcuts
+map('n', '<leader>fc', function()
+    builtin.find_files({ cwd = '../common' })
+end, { desc = 'Find files in ../common' })
+
+map('n', '<leader>fbp', function()
+    builtin.find_files({ cwd = '../../boards/arm' })
+end, { desc = 'Find files in ../../boards/arm' })
+
+--========================================================
+-- Plugin Configs
+--========================================================
+require('oil').setup({ default_file_explorer = false })
+require('which-key').setup({ delay = 300, show_help = true })
 require('mini.bufremove').setup()
--- require('plugins.devroots')
 
--- UI
+--========================================================
+-- UI & Misc
+--========================================================
 vim.cmd("colorscheme vague")
-vim.cmd("hi statusline guibg=none")
+vim.cmd("hi StatusLine guibg=none")
 
--- Yank highlight
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
-    desc = 'Highlight selection on yank',
+    desc = 'Highlight on yank',
     callback = function()
         vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 500 })
     end,
 })
+
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "help",
+    pattern = { "help", "new" },
     callback = function()
-        vim.cmd("wincmd o")
-        vim.cmd("resize")    -- resize full height
-        vim.opt_local.buflisted = true
-    end,
-})
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "new",
-    callback = function()
-        vim.cmd("wincmd o")  -- closes other windows
-        vim.cmd("resize")    -- resize full height
+        vim.cmd("wincmd o | resize")
         vim.opt_local.buflisted = true
     end,
 })

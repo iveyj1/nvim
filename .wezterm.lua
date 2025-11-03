bfce801cb7c042f9c12a753064df14068fc29e44
Jack Ivey <iveyj@hotmail.com>
2025-11-02 21:40:33 -0500

    add tmux, wezterm conf

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.enable_scroll_bar = true
+
+-- Load the built-in rose-pine scheme
+local rose_pine = wezterm.color.get_builtin_schemes()['rose-pine']
+
+-- Modify a specific color (e.g., background)
+rose_pine.background = '#101010'
+
+-- Apply the modified colors
+config.colors = rose_pine
+
+return config   
